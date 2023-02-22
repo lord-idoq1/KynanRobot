a7d8e621a02823dccaed4fe47494c06bad9a1ef4
lord-idoq1 <90918779+lord-idoq1@users.noreply.github.com>
2023-02-23 02:20:05 +0700

    Update __init__.py

--- KynanRobot/__init__.py
+++ KynanRobot/__init__.py
@@ -60,7 +60,7 @@ if ENV:
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
     START_IMG = os.environ.get(
-        "START_IMG", "https://telegra.ph//file/b5fa050775543872ae0ec.jpg"
+        "START_IMG", "https://te.legra.ph/file/1d256ef33445db17e3513.jpg"
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "kynansupport")
